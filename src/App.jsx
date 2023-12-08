05b61eb944ac3923f69c49137c3d98df85dfe1b3
unknown <victorjayeoba02@gmail.com>
2023-12-08 14:34:59 +0100

    adding commands

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { FaHeart, FaSearch } from "react-icons/fa";
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
-<div>
-  <div>
-  <h1 className='font-primary block text-2xl'></h1>
-  </div>
-  <div>
-  <FaSearch style={{color: 'red', fontSize: '50px'}}/>
-  </div>
-</div>
-
-  )
+    <div>
+      <div>
+        <h1 className="font-primary block text-2xl"></h1>
+      </div>
+      <div>
+        <FaSearch style={{ color: "red", fontSize: "50px" }} />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
+// git pull origin master
+//npm run dev
+//git add .
+// git commit -m "bla bla bla"
+//git push
