12efe2a2a414516c00281ec3e9f1a978b863c41d
Temitope Akinsunmade <121050758+Temitope15@users.noreply.github.com>
2024-01-01 22:02:48 +0000

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,26 +6,7 @@ import SearchBox from "./SearchBox";
 import { motion, AnimatePresence, delay } from "framer-motion";
 
 function Navbar() {
-  /*
-const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 0, x: "-100%" },
-}
 
-export const MyComponent = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  return (
-    <motion.nav
-      animate={isOpen ? "open" : "closed"}
-      variants={variants}
-    >
-      <Toggle onClick={() => setIsOpen(isOpen => !isOpen)} />
-      <Items />
-    </motion.nav>
-  )
-}
-*/
 
   const variants = {
     open: { opacity: 1, scaleY: 1 },
